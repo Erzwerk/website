2c8db2f1c2d9e69b7800605bf04f9579f6d0910f
Erzwerk <82177762+Erzwerk@users.noreply.github.com>
2021-05-15 15:31:09 +0200

    Update HardwarekombinationJS.js

--- htdocs/Hardwarekombinationen/HardwarekombinationJS.js
+++ htdocs/Hardwarekombinationen/HardwarekombinationJS.js
@@ -1,15 +1,60 @@
+
 function was_clicked () {
-    console.log("clicked!");
+	console.log("clicked!");
+        
+        
+        var cpu = document.getElementById("HardwareCPU");
+        console.log(cpu.options[cpu.selectedIndex].value);
+        var CPU = cpu.options[cpu.selectedIndex].value;
+        
+        var gpu = document.getElementById("HardwareGPU");
+        console.log(gpu.options[gpu.selectedIndex].value);
+        var GPU = gpu.options[gpu.selectedIndex].value;
+        
+        var dateipfad = CPU + "/" + GPU + "/Bild.png";
+        console.log (dateipfad);
+    
+        var element = document.getElementById("Bild").firstElementChild;
+        if (element != null){
+            element.parentNode.removeChild(element);
+        }
+    
+        var img = document.createElement("img");
+        img.src = dateipfad;
+        document.getElementById("Bild").appendChild(img);
 
-    var cpu = document.getElementById("HardwareCPU");
-    console.log(cpu.options[cpu.selectedIndex].value);
-    var CPU = cpu.options[cpu.selectedIndex].value;
+    
+}
+document.getElementById("Execute").addEventListener("click", was_clicked);
 
-    var gpu = document.getElementById("HardwareGPU");
-    console.log(gpu.options[gpu.selectedIndex].value);
-    var GPU = gpu.options[gpu.selectedIndex].value;
+function was_saved () {
+    console.log("saved");
+        var element = document.getElementById("Bild").firstElementChild;
+        if (element != null){
+            
+        var cpu = document.getElementById("HardwareCPU");
+        var CPU = cpu.options[cpu.selectedIndex].value;
+        
+        var gpu = document.getElementById("HardwareGPU");
+        var GPU = gpu.options[gpu.selectedIndex].value;
+        
+        var speichern = CPU + "/" + GPU + "/Bild.png";
+        console.log(speichern);
 
-    var dateipfad = CPU + "/" + GPU + "/Bild.png";
+        document.cookie = speichern + "; expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=Lax";
+        }
+        else {
+            throw ("select a benchmark");
+        }
+}
+document.getElementById("save").addEventListener("click", was_saved);
+
+function was_tested(){
+    console.log("1");
+    var speichern = document.cookie;
+    console.log(speichern);
+
+    var dateipfad = speichern;
     console.log (dateipfad);
 
     var element = document.getElementById("Bild").firstElementChild;
@@ -20,40 +65,6 @@ function was_clicked () {
     var img = document.createElement("img");
     img.src = dateipfad;
     document.getElementById("Bild").appendChild(img);
-
-}
-document.getElementById("Execute").addEventListener("click", was_clicked);
-
-function was_saved () {
-    console.log("saved");
-    var element = document.getElementById("Bild").firstElementChild;
-    if (element != null){
-
-        var cpu = document.getElementById("HardwareCPU");
-        var CPU = cpu.options[cpu.selectedIndex].value;
-
-        var gpu = document.getElementById("HardwareGPU");
-        var GPU = gpu.options[gpu.selectedIndex].value;
-
-        var speichern = CPU + "/" + GPU + "/Bild.png";
-        console.log(speichern);
-
-        document.cookie = "username=Patrick;";
-        document.cookie = "config=" + speichern + ";";
-    }
-    else {
-        throw ("select a benchmark");
-    }
-}
-document.getElementById("save").addEventListener("click", was_saved);
-
-function was_tested(){
-    console.log("1");
-    var speichern = document.cookie;
-    console.log(speichern);
-    var username = document.cookie;
-    console.log(username);
-
-}
+    } 
 
 document.getElementById("test").addEventListener("click", was_tested);
