ab7e6702e06b8ac527dd53899d43661811113f46
ArkussGer <83020596+ArkussGer@users.noreply.github.com>
2021-05-23 13:55:51 +0200

    Update 700-900.php

--- htdocs/Hardwarekombinationen/700-900.php
+++ htdocs/Hardwarekombinationen/700-900.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title> Gamingfacts - Hardwarekonfiguration 500-700 </title>
+    <title> Gamingfacts - Hardwarekonfiguration 700-900 </title>
     <link rel="stylesheet" type="text/css" href="../Navbar/Style.css"/>
     <link rel="stylesheet" type="text/css" href="../HeaderDesign.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="icon" type="image/png" href="../ProjektLogo.png">
     <meta name="keywords" content="Hardware, CPU, GPU" />
-    <meta name="description" content="Eine Website, welche Hardwarekonfigurationen im Preisbereich 500€ - 700€ beinhaltet." />
+    <meta name="description" content="Eine Website, welche Hardwarekonfigurationen im Preisbereich 700€ - 900€ beinhaltet." />
 
 </head>
 
