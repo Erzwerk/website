4a8364014318430237faab96cf7c0906b57c7d46
ArkussGer <83020596+ArkussGer@users.noreply.github.com>
2021-05-23 13:58:48 +0200

    Update 1500-2000.php

--- htdocs/Hardwarekombinationen/1500-2000.php
+++ htdocs/Hardwarekombinationen/1500-2000.php
@@ -2,13 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title> Gamingfacts - Hardwarekonfiguration 500-700 </title>
+    <title> Gamingfacts - Hardwarekonfiguration 1500-2000 </title>
     <link rel="stylesheet" type="text/css" href="../Navbar/Style.css"/>
     <link rel="stylesheet" type="text/css" href="../HeaderDesign.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" type="text/css" href="HKDesign.css"/>
     <link rel="icon" type="image/png" href="../ProjektLogo.png">
     <meta name="keywords" content="Hardware, CPU, GPU" />
-    <meta name="description" content="Eine Website, welche Hardwarekonfigurationen im Preisbereich 500€ - 700€ beinhaltet." />
+    <meta name="description" content="Eine Website, welche Hardwarekonfigurationen im Preisbereich 1500€ - 2000€ beinhaltet." />
 
 </head>
 
