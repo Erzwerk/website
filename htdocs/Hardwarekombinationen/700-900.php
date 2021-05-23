1f945e71c6159fac0d8063d46d62ca96e2ebf9a8
ArkussGer <83020596+ArkussGer@users.noreply.github.com>
2021-05-23 13:57:46 +0200

    Update 700-900.php

--- htdocs/Hardwarekombinationen/700-900.php
+++ htdocs/Hardwarekombinationen/700-900.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" type="text/css" href="../Navbar/Style.css"/>
     <link rel="stylesheet" type="text/css" href="../HeaderDesign.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" type="text/css" href="HKDesign.css"/>
     <link rel="icon" type="image/png" href="../ProjektLogo.png">
     <meta name="keywords" content="Hardware, CPU, GPU" />
     <meta name="description" content="Eine Website, welche Hardwarekonfigurationen im Preisbereich 700€ - 900€ beinhaltet." />
