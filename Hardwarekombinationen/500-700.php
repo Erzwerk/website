0a70143822f25f359fb9b8c5131dfbc44fe7a9f5
ArkussGer <83020596+ArkussGer@users.noreply.github.com>
2021-05-28 23:20:44 +0200

    Update 500-700.php

--- Hardwarekombinationen/500-700.php
+++ Hardwarekombinationen/500-700.php
@@ -5,38 +5,40 @@
     <title> Gamingfacts - Hardwarekonfiguration 500-700 </title>
     <link rel="stylesheet" type="text/css" href="../Navbar/Style.css"/>
     <link rel="stylesheet" type="text/css" href="../HeaderDesign.css"/>
-    <link rel="stylesheet" type="text/css" href="FKDesign.css"/>
+    <link rel="stylesheet" type="text/css" href="HKDesign.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="icon" type="image/png" href="../ProjektLogo.png">
     <meta name="keywords" content="Hardware, CPU, GPU" />
     <meta name="description" content="Eine Website, welche Hardwarekonfigurationen im Preisbereich 500€ - 700€ beinhaltet." />
 </head>
 
-        <?php
+    <?php
         include "../header.html";
         include "Navbar57.php";
-        ?>
+    ?>
+
 <body>
-<section>
-    Mit diesem PC sollte es möglich sein alle aktuellen Spiele in mittleren bis hohen Einstellungen<br>
-    mit 1080p absolut flüssig spielen zu können. Aufgrund der neuen AM4-Plattform ist dieser PC<br>
-    außerdem sehr zukunftssicher und stellt die perfekte Basis dar um später aufrüsten zu können!<br>
-    In dieser Zusammenstellung ist alles perfekt auf einander abgestimmt!<br>
-    Die Komponenten passen 100% zusammen und das System harmoniert hinsichtlich der Auswahl und Leistung.<br>
-    Das komplette Zubehör für den Zusammenbau (Kabel usw.) ist schon dabei – es wird nichts weiter benötigt!<br>
-    <br>
-    - Counter Strike (CS:GO): 180fps @FullHD und hohe Einstellungen <br>
-    - Overwatch: 130fps @FullHD und hohe Einstellungen <br>
-    - Fortnite Battle Royale: 110fps @FullHD und hohe Einstellungen<br>
-    - Playerunknowns Battlegrounds (PUBG): 80fps @FullHD und niedrige - mittlere Einstellungen<br>
-    - Battlefield 5 (BF5): 70fps @FullHD und hohe Einstellungen<br>
-    - Grand Theft Auto 5 (GTA5): 75fps @FullHD und hohe Einstellungen<br>
-    - Witcher 3: 80fps @FullHD und hohe Einstellungen<br>
-    - Cinebench R15 (Multi): 1300 Punkte<br>
-    - 3DMark Firestrike: 13000 Punkte<br>
-    <br>
-</section>
-<table>
+    <section>
+        <h2> Erklärung </h2>
+        Mit diesem PC sollte es möglich sein alle aktuellen Spiele in mittleren bis hohen Einstellungen<br>
+        mit 1080p absolut flüssig spielen zu können. Aufgrund der neuen AM4-Plattform ist dieser PC<br>
+        außerdem sehr zukunftssicher und stellt die perfekte Basis dar um später aufrüsten zu können!<br>
+        In dieser Zusammenstellung ist alles perfekt auf einander abgestimmt!<br>
+        Die Komponenten passen 100% zusammen und das System harmoniert hinsichtlich der Auswahl und Leistung.<br>
+        Das komplette Zubehör für den Zusammenbau (Kabel usw.) ist schon dabei – es wird nichts weiter benötigt!<br>
+        <br>
+        <h2> Performance </h2>
+        - Counter Strike (CS:GO): 180fps @FullHD und hohe Einstellungen <br>
+        - Overwatch: 130fps @FullHD und hohe Einstellungen <br>
+        - Fortnite Battle Royale: 110fps @FullHD und hohe Einstellungen<br>
+        - Playerunknowns Battlegrounds (PUBG): 80fps @FullHD und niedrige - mittlere Einstellungen<br>
+        - Battlefield 5 (BF5): 70fps @FullHD und hohe Einstellungen<br>
+        - Grand Theft Auto 5 (GTA5): 75fps @FullHD und hohe Einstellungen<br>
+        - Witcher 3: 80fps @FullHD und hohe Einstellungen<br>
+        - Cinebench R15 (Multi): 1300 Punkte<br>
+        - 3DMark Firestrike: 13000 Punkte<br>
+    </section>
+<table style="margin: auto">
     <tr> <th>Bauteil</th>        <th>Bezeichnung</th>                    <th>Link</th> </tr>
     <tr> <td>Prozessor</td>   <td>Intel Core i3-10100F</td>           <td> <a href = https://www.caseking.de/intel-core-i3-10100f-3-60-ghz-comet-lake-s-sockel-1200-boxed-hpit-692.html?sPartner=185&utm_source=geizhals&utm_medium=comparison&utm_campaign=PC-Komponenten+%3E+CPUs+%2F+Prozessoren+%3E+Intel&campaign=psm/geizhals&wt_mc=preisvergleich.geizhals.feed> Hier Klicken </a> </td> </tr>
     <tr> <td>Mainboard</td>   <td>MSI B460M Pro-VDH</td>              <td> <a href = https://www.mindfactory.de/product_info.php/MSI-B460M-Pro-VDH--So-1200-micro-ATX--7C83-026R-_1381077.html> Hier Klicken </a> </td> </tr>
@@ -46,6 +48,7 @@
     <tr> <td>Netzteil</td>    <td>400W be quiet! System Power 9</td>  <td> <a href = https://www.mindfactory.de/product_info.php/400-Watt-be-quiet--System-Power-9-Non-Modular-80--Bronze_1228435.html> Hier Klicken </a> </td> </tr>
     <tr> <td>Gehäuse</td>     <td>Antec NX100</td>                    <td> <a href = https://www.proshop.de/Gehaeuse/Antec-NX100-mid-tower-ATX-Gehaeuse-Miditower-Grau/2778576?utm_source=geizhals&utm_medium=cpc&utm_campaign=pricesite> Hier Klicken </a> </td> </tr>
 </table>
-<a href = https://www.hardwaredealz.com/bester-gaming-pc-fuer-unter-500-euro-gamer-pc-bis-500-euro>Quelle</a><br>
+    <a class="Quelle" href = https://www.hardwaredealz.com/bester-gaming-pc-fuer-unter-500-euro-gamer-pc-bis-500-euro>Quelle</a><br>
+
 </body>
 </html>
